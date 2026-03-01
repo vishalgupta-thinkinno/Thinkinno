bc5d24fba4a2919ca9893de6f104c2e3db83a2ad
vishalgupta-thinkinno <vishal.gupta@thinkinno.com>
2026-03-01 22:41:43 +0530

    Improve fetch error handling and include Footer
    Refactor fetch logic to handle AbortError and added Footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
 import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Header from './components/Header';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { textSliceActions } from './store/textContentSlice';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Outlet } from 'react-router-dom';
 import About from './components/About';
+import Footer from "./components/Footer";
+
 function App() {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    fetch('/content.json', {signal})
-    .then(res => res.json())
-    .then(result => {
-      dispatch(textSliceActions.addInitialText(result));
-    })
-    .catch(err => {
-      err.name ==='Abort'? "" : "";
-    })
-    return ()=> {
+    fetch('/content.json', { signal })
+      .then(res => res.json())
+      .then(result => {
+        dispatch(textSliceActions.addInitialText(result));
+      })
+      .catch(err => {
+        if (err.name !== 'AbortError') {
+          console.error(err);
+        }
+      });
+
+    return () => {
       controller.abort();
-    }
-  }, []); 
+    };
+  }, [dispatch]);
 
   return (
     <>
-    <Header />
-    <Outlet/>
-    <About />
-    {/* <Footer /> */}
+      {/* <Header /> */}
+      {/* <Outlet /> */}
+      <About/>
+      <Footer/>
     </>
   );
 }
